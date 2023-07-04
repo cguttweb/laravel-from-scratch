1e7c857399275e219c4c48d11dea94d4d4c6c552
Chloe <cguttweb@pm.me>
2023-07-04 20:53:10 +0100

    two different options for arrows

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -18,15 +18,10 @@
             <x-slot name="trigger">
                 <button class="inline-flex py-2 pl-3 pr-9 text-left text-sm font-semibold w-full lg:w-32">
                     {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
-                    <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
-                    height="22" viewBox="0 0 22 22">
-                        <g fill="none" fill-rule="evenodd">
-                            <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
-                            </path>
-                            <path fill="#222"
-                                    d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
-                        </g>
-                    </svg>
+                    {{-- option 1 --}}
+                    {{-- <x-down-arrow class="absolute pointer-events-none" style="right: 12px;" /> --}}
+                    {{-- option 2 more generic if multiple --}}
+                    <x-icon name="down-arrow" class="absolute pointer-events-none" style="right: 12px;" />
                 </button>
             </x-slot>
 
