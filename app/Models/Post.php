bc900b557483ea70c94b4e6048499b1e251f41a6
Chloe <cguttweb@pm.me>
2023-07-05 21:38:55 +0100

    search less messyu way

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -26,11 +26,11 @@ class Post extends Model
     public function scopeFilter($query, array $filters) {
         // Post::newQuery()->filter()->where('title'....)
         // $query->where
-           if($filters['search'] ?? false){
+        $query->when($filters['search'] ?? false, function ($query, $search){
             $query
-            ->where('title', 'like', '%' . request('search') . '%')
-            ->orWhere('body', 'like', '%' . request('search') . '%');
-        }
+            ->where('title', 'like', '%' . $search . '%')
+            ->orWhere('body', 'like', '%' . $search . '%');
+         });
     }
 
     public function category(){
