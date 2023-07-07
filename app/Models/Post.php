7ab0792f9e5596bb6cf3fbbd2453460db4cbd40d
Chlo <cguttridge@clever-touch.com>
2023-07-07 12:16:09 +0100

    updated post model to inc closure around search to fix bug

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -27,9 +27,10 @@ class Post extends Model
         // Post::newQuery()->filter()->where('title'....)
         // $query->where
         $query->when($filters['search'] ?? false, function ($query, $search){
-            $query
-            ->where('title', 'like', '%' . $search . '%')
-            ->orWhere('body', 'like', '%' . $search . '%');
+            $query->where(fn($query) =>
+                $query->where('title', 'like', '%' . $search . '%')
+                ->orWhere('body', 'like', '%' . $search . '%')
+            );
          });
 
          $query->when($filters['category'] ?? false, fn ($query, $category) =>
