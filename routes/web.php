92bcc524eb334a0806bfab06b930ca87b5560d19
Chloe <cguttweb@pm.me>
2023-07-09 21:22:42 +0100

    looking at mailchimp api

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,24 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\SessionController;
 use App\Http\Controllers\RegisterController;
 
+
+Route::get('ping', function(){
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us8'
+    ]);
+
+    // $response = $mailchimp->lists->getAllLists(); // at this point just one list so single id returned b09591bd3a
+    $response = $mailchimp->lists->addListMember('b09591bd3a', [
+        "email_address" => "prudence.mcvankab@example.com",
+        "status" => "subscribed",
+    ]);
+    ddd($response);
+
+});
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 // route model binding = binding route key to underlying eloquent model
